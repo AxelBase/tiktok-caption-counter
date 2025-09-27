b3e6c335becf6f7805cf24514cd1dcf7dff960e9
Sahil Ramjeeth <22046468@dut4life.ac.za>
2025-09-27 21:25:44 +0200

    GSC Verification post build

--- docs/_app/immutable/chunks/BBbfmox7.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_t7v6pj?.base??"/tiktok-caption-counter",t=globalThis.__sveltekit_t7v6pj?.assets??s??"";export{t as a,s as b};
---
+++ docs/_app/immutable/chunks/Y9Xv7u6o.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_paot02?.base??"/tiktok-caption-counter",t=globalThis.__sveltekit_paot02?.assets??s??"";export{t as a,s as b};
