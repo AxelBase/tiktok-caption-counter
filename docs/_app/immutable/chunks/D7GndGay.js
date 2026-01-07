fd9f95c90f65ea16f3bea870b0a43155a0c34463
Sahil Ramjeeth <22046468@dut4life.ac.za>
2026-01-07 20:14:46 +0200

    Fix B

---
+++ docs/_app/immutable/chunks/D7GndGay.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_cg8pki?.base??"/tiktok-caption-counter",t=globalThis.__sveltekit_cg8pki?.assets??s??"";export{t as a,s as b};
--- docs/_app/immutable/chunks/OWjOnoQq.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_aq96ia?.base??"/tiktok-caption-counter",a=globalThis.__sveltekit_aq96ia?.assets??s??"";export{a,s as b};
